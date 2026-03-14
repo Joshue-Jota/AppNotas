5740aa766a65289abbc52e2664bfdd3f149e14b0
Joshue-Jota <joshuecharles@gmail.com>
2026-03-13 20:21:01 -0500

    Metodos de visualizar y guardar las notas

---
+++ Notas/ViewModels/NotasViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Notas.Database;
+using Notas.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Notas.ViewModels
+{
+    public class NotasViewModel
+    {
+        public ObservableCollection<Nota> ListaNotas { get; set; }
+
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+
+        public ICommand GuardarCommand { get; }
+
+        DatabaseNotas database;
+
+        public NotasViewModel(DatabaseNotas db)
+        {
+            database = db;
+            ListaNotas = new ObservableCollection<Nota>();
+
+            GuardarCommand = new Command(async () => await GuardarNota());
+
+            CargarNotas();
+        }
+
+        async void CargarNotas()
+        {
+            var notas = await database.GetNotasAsync();
+
+            foreach (var nota in notas)
+                ListaNotas.Add(nota);
+        }
+
+        async Task GuardarNota()
+        {
+            var nuevaNota = new Nota
+            {
+                Titulo = Titulo,
+                Contenido = Contenido
+            };
+
+            await database.SaveNotaAsync(nuevaNota);
+            ListaNotas.Add(nuevaNota);
+
+            Titulo = "";
+            Contenido = "";
+        }
+    }
+}
