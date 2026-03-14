d5c3615f44e96a63a02e43c16df96e9b7bb582a1
Joshue-Jota <joshuecharles@gmail.com>
2026-03-13 20:13:31 -0500

    Creacion del archivo de la base de datos

---
+++ Notas/Database/DatabaseNotas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Notas.Models;
+
+namespace Notas.Database
+{
+    public class DatabaseNotas
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public DatabaseNotas(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Nota>().Wait();
+        }
+
+        public Task<List<Nota>> GetNotasAsync()
+        {
+            return database.Table<Nota>().ToListAsync();
+        }
+
+        public Task<int> SaveNotaAsync(Nota nota)
+        {
+            return database.InsertAsync(nota);
+        }
+
+        public Task<int> DeleteNotaAsync(Nota nota)
+        {
+            return database.DeleteAsync(nota);
+        }
+    }
+}
+
